8cf05c469093e199f5b8ee39ee50d71503845a1e
Joy <biwott.joy.03@gmail.com>
2023-09-07 22:35:27 +0300

    Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <navbar className="header">
+             <img src="../public/images/troll-face.png" className="troll-face"/>
+             <h3>Meme Generator</h3>
+             <p className="react--course">React Course - Project 3</p>
+        </navbar>
+    )
+}
