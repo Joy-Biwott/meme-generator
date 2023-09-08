9558103e1d5fd6c54facc28fe0c4fd4972581440
Joy <biwott.joy.03@gmail.com>
2023-09-08 20:33:48 +0300

    s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header(){
     return(
         <navbar className="header">
              <img src="../public/images/troll-face.png" className="troll-face"/>
-             <h3>Meme Generator</h3>
-             <p className="react--course">React Course - Project 3</p>
+             <h2 className="header--title">Meme Generator</h2>
+             <h4 className="header--project">React Course - Project 3</h4>
         </navbar>
     )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,15 +9,20 @@ import React from "react";
 
 export default function Meme (){
     return (
-        <div>
-            <form>
-                <div className="input">
-                   <input type="text" />
-                   <input type="text" className="input-2"/><br/> 
-                </div>
-                
-                <button>Get a new meme image</button>
+        <main>
+            <form className="form">
+                   <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Top text"
+                   />
+                   <input 
+                    type="text" 
+                    className="form--input"
+                    placeholder="Bottom text"
+                   />                
+                <button className="form--button">Get a new meme image 🖼</button>
             </form>
-        </div>
+        </main>
     )
 }
