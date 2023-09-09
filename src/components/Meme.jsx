105d6f7b7cd8052901f89a0c1dd0ef212c58f75a
Joy <biwott.joy.03@gmail.com>
2023-09-09 14:31:06 +0300

    updated meme-image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import memes from "../memesData.js";
+
 /**
- * Challenge: 
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+
 console.log(memes)
 export default function Meme (){
+    const [image, setImage] = React.useState("")
     function getMemeImage(){
-        const memesArr= memes.data.memes.map(())
+        const memesArr= memes.data.memes
         const randomNum = Math.floor(Math.random() * memesArr.length)
         const url = memesArr[randomNum].url
-        return <img src={url}  alt="meme"/>
+        setImage(url)
+        
     }
     return (
         <main>
@@ -29,6 +36,7 @@ export default function Meme (){
                     placeholder="Bottom text"
                    />                
                 <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
+                <img src={image}  alt="meme" className="meme--image"/>
             </div>
         </main>
     )
